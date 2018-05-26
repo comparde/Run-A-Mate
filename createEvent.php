e9944730ef8052b5c6af4c3b4da15ad23d66291c
Pelle1994 <pelle_backstrom94@hotmail.com>
2018-05-26 12:49:01 +0200

    lagt till hannes php validering

--- createEvent.php
+++ createEvent.php
@@ -1,15 +1,36 @@
 <?php
-     include 'db.php';
-     $name = prepareString($_POST["eventName"]);
-     $description = prepareString($_POST["descr"]);
-     $location = prepareString($_POST["location"]);
-     $date = ($_POST["date"]);
-	 $skillLevel = prepareString($_POST['skillLevel']);
-     session_start();
-     $ID = $_SESSION['ID'];
-     createEvent($name, $description, $location, $date, $ID, $skillLevel);
-
+    include 'db.php';
+    $name = test_input(prepareString($_POST["eventName"]));
+    $description = test_input(prepareString($_POST["descr"]));
+    $location = test_input(prepareString($_POST["location"]));
+    $date = test_input($_POST["date"]);
+	$skillLevel = test_input(prepareString($_POST['skillLevel']));
+    session_start();
+    $ID = $_SESSION['ID'];
 	
-
-     header("Location: _runEvents.php");
+	if (strlen($name) == 0)
+	{
+		echo "The event needs a name!";
+		header("Refresh: 3; url=eventCreater.php");
+	}
+	else if (strlen($description) == 0)
+	{
+		echo "The event needs a description!";
+		header("Refresh: 3; url=eventCreater.php");
+	}
+	else if (strlen($location) == 0)
+	{
+		echo "The event needs a location!";
+		header("Refresh: 3; url=eventCreater.php");
+	}
+	else if (strlen($date) == 0)
+	{
+		echo "The event needs a date!";
+		header("Refresh: 3; url=eventCreater.php");
+	}
+	else
+	{
+		createEvent($name, $description, $location, $date, $ID, $skillLevel);
+		header("Location: _runEvents.php");
+	}
  ?>
